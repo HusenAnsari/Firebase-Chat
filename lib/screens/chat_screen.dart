e6b98582e77f380d72f2a7564d0024d1976ae742
Husen Ansari <husenansari07@gmail.com>
2020-05-08 12:40:18 +0530

    Firebase setup, Firestore database configuration, get demo data from firestore in application

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +11,18 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text("This work"),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/zIebG2EiCXXIOlFrJOdf/messages')
+              // snapshots() return stream that allow as to get latest data everytime
+              .snapshots()
+              .listen((data) {
+            print(data.documents[0]['text']);
+          });
+        },
       ),
     );
   }
